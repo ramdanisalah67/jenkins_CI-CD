7cf23c94119ce3b10300f7f38a38004c9200bd53
asmaelakhal09 <Asmaelakhal127@gmail.com>
2024-08-03 14:16:14 +0100

    second commit

--- src/main/java/com/example/demo/ForumAppApplication.java
+++ src/main/java/com/example/demo/ForumAppApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.Models.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class ForumAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumAppApplication.class, args);
 	}
 
+
+
+	@Bean
+	public void initMessage(){
+
+		log.info("init Message");
+	}
 }
