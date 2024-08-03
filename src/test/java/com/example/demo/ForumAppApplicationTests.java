c8ccbaabac36603bab146a95243deb006b5b9383
asmaelakhal09 <Asmaelakhal127@gmail.com>
2024-08-03 21:22:32 +0100

    add test method

--- src/test/java/com/example/demo/ForumAppApplicationTests.java
+++ src/test/java/com/example/demo/ForumAppApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class ForumAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testLengthName() {
+		assertEquals(5,"salah".length());
 	}
 
 }
